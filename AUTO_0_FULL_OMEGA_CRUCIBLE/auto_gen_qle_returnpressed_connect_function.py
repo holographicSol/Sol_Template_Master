1b013aad9c04dccc8d5ffb799b9a8f48739530fe
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2022-12-14 06:42:28 +0000

    Add files via upload

---
+++ AUTO_0_FULL_OMEGA_CRUCIBLE/auto_gen_qle_returnpressed_connect_function.py
@@ -0,0 +1,257 @@
+import datetime
+##import sol_template_master
+
+var = []
+
+def qlex_0_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_0'))
+
+def qlex_1_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_1'))
+
+def qlex_2_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_2'))
+
+def qlex_3_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_3'))
+
+def qlex_4_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_4'))
+
+def qlex_5_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_5'))
+
+def qlex_6_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_6'))
+
+def qlex_7_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_7'))
+
+def qlex_8_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_8'))
+
+def qlex_9_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_9'))
+
+def qlex_10_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_10'))
+
+def qlex_11_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_11'))
+
+def qlex_12_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_12'))
+
+def qlex_13_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_13'))
+
+def qlex_14_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_14'))
+
+def qlex_15_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_15'))
+
+def qlex_16_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_16'))
+
+def qlex_17_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_17'))
+
+def qlex_18_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_18'))
+
+def qlex_19_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_19'))
+
+def qlex_20_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_20'))
+
+def qlex_21_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_21'))
+
+def qlex_22_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_22'))
+
+def qlex_23_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_23'))
+
+def qlex_24_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_24'))
+
+def qlex_25_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_25'))
+
+def qlex_26_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_26'))
+
+def qlex_27_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_27'))
+
+def qlex_28_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_28'))
+
+def qlex_29_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_29'))
+
+def qlex_30_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_30'))
+
+def qlex_31_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_31'))
+
+def qlex_32_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_32'))
+
+def qlex_33_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_33'))
+
+def qlex_34_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_34'))
+
+def qlex_35_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_35'))
+
+def qlex_36_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_36'))
+
+def qlex_37_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_37'))
+
+def qlex_38_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_38'))
+
+def qlex_39_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_39'))
+
+def qlex_40_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_40'))
+
+def qlex_41_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_41'))
+
+def qlex_42_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_42'))
+
+def qlex_43_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_43'))
+
+def qlex_44_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_44'))
+
+def qlex_45_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_45'))
+
+def qlex_46_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_46'))
+
+def qlex_47_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_47'))
+
+def qlex_48_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_48'))
+
+def qlex_49_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_49'))
+
+def qlex_50_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_50'))
+
+def qlex_51_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_51'))
+
+def qlex_52_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_52'))
+
+def qlex_53_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_53'))
+
+def qlex_54_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_54'))
+
+def qlex_55_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_55'))
+
+def qlex_56_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_56'))
+
+def qlex_57_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_57'))
+
+def qlex_58_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_58'))
+
+def qlex_59_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_59'))
+
+def qlex_60_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_60'))
+
+def qlex_61_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_61'))
+
+def qlex_62_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_62'))
+
+def qlex_63_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_63'))
+
+def qlex_64_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_64'))
+
+def qlex_65_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_65'))
+
+def qlex_66_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_66'))
+
+def qlex_67_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_67'))
+
+def qlex_68_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_68'))
+
+def qlex_69_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_69'))
+
+def qlex_70_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_70'))
+
+def qlex_71_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_71'))
+
+def qlex_72_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_72'))
+
+def qlex_73_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_73'))
+
+def qlex_74_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_74'))
+
+def qlex_75_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_75'))
+
+def qlex_76_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_76'))
+
+def qlex_77_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_77'))
+
+def qlex_78_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_78'))
+
+def qlex_79_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_79'))
+
+def qlex_80_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_80'))
+
+def qlex_81_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_81'))
+
+def qlex_82_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_82'))
+
+def qlex_83_returnPressed_connect_function():
+    print(str('[' + str(datetime.datetime.now()) + '] return pressed: qlex_83'))
+
---
+++ AUTO_0_FULL_OMEGA_CRUCIBLE/sol_template_master.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run chr(34) & "D:\Documents\Work\1. Projects\0_Template_Manufacturing\gui_sol_template_omega{mech}\CHECKPOINT_EXAMPLES\AUTO_0_FULL_OMEGA_BOTTOM_LEFT_RIGHT_RESERVED\sol_template_master.exe" & Chr(34), 0
+Set WshShell = Nothing
